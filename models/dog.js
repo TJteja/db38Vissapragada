1a5203877bdb063573379ccc2583cf0e81f2a35e
Sai Tejaswee Vissapragada <teja108116@gmail.com>
2021-11-10 16:09:43 -0600

    Update dog.js

--- models/dog.js
+++ models/dog.js
@@ -1,4 +1,4 @@
 const mongoose = require("mongoose") 
-const dogSchema = mongoose.Schema({  breed: String,  price: Number,  color: String }) 
+const dogSchema = mongoose.Schema({  breed: String,  price: Number,  colour: String }) 
  
 module.exports = mongoose.model("dog", dogSchema) 
