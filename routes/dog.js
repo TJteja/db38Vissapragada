cc4aa92af14e51bc82c268ed8b76099dd4ac6b03
Sai Tejaswee Vissapragada <teja108116@gmail.com>
2021-11-10 18:32:02 -0600

    Update dog.js

--- routes/dog.js
+++ routes/dog.js
@@ -3,6 +3,8 @@ const dog_controllers= require('../controllers/dog');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', dog_controllers.dog_view_all_Page ); 
+router.get('/', function(req, res, next) {
+  res.render('dog', { title: 'Search Results dog' });
+});
 
 module.exports = router;
