89d738022613742748c3de94fb41445f749957a5
Sai Tejaswee Vissapragada <teja108116@gmail.com>
2021-11-11 23:47:58 -0600

    Update dog.js

--- controllers/dog.js
+++ controllers/dog.js
@@ -24,8 +24,15 @@ exports.dog_view_all_Page = async function(req, res) {
     }   
 }; 
 // for a specific dog. 
-exports.dog_detail = function(req, res) { 
-    res.send('NOT IMPLEMENTED: dog detail: ' + req.params.id); 
+exports.dog_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await Dog.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
 }; 
  
 // Handle dog delete form on DELETE. 
@@ -34,8 +41,24 @@ exports.dog_delete = function(req, res) {
 }; 
  
 // Handle dog update form on PUT. 
-exports.dog_update_put = function(req, res) { 
-    res.send('NOT IMPLEMENTED: dog update PUT' + req.params.id); 
+exports.dog_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await Dog.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.breed)  
+               toUpdate.breed = req.body.breed; 
+        if(req.body.price) toUpdate.price = req.body.price; 
+        if(req.body.size) toUpdate.colour = req.body.colour; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`); 
+    }  
 }; 
 // Handle Costume create on POST. 
 exports.dog_create_post = async function(req, res) { 
@@ -56,4 +79,4 @@ exports.dog_create_post = async function(req, res) {
         res.status(500); 
         res.send(`{"error": ${err}}`); 
     }   
-}; 
+};
