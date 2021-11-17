aea1146e7170e4ff0a31ce3e282de537ec990f20
Sai Tejaswee Vissapragada <teja108116@gmail.com>
2021-11-17 09:33:21 -0600

    Update dog.js

--- routes/dog.js
+++ routes/dog.js
@@ -4,8 +4,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', dog_controllers.dog_view_all_Page);
-
-module.exports = router;
 /* GET detail dog page */ 
 router.get('/detail', dog_controllers.dog_view_one_Page); 
 /* GET create costume page */ 
@@ -14,4 +12,6 @@ router.get('/create', dog_controllers.dog_create_Page);
 router.get('/update', dog_controllers.dog_update_Page); 
 /* GET delete costume page */ 
 router.get('/delete', dog_controllers.dog_delete_Page); 
+module.exports = router;
+
  
