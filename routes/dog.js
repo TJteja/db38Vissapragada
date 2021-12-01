f042cf928b20a95022e62f9fad655113d45635dc
Sai Tejaswee Vissapragada <teja108116@gmail.com>
2021-12-01 15:24:47 -0600

    m

--- routes/dog.js
+++ routes/dog.js
@@ -9,7 +9,7 @@ router.get('/detail', dog_controllers.dog_view_one_Page);
 /* GET create costume page */ 
 router.get('/create', dog_controllers.dog_create_Page); 
 /* GET create update page */ 
-router.get('/update', dog_controllers.dog_update_Page); 
+router.get('/update',secured, dog_controllers.dog_update_Page); 
 /* GET delete costume page */ 
 router.get('/delete', dog_controllers.dog_delete_Page); 
 module.exports = router;
